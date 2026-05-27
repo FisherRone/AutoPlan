4b5c98a5ad26efdbffb30391a02dfc507e27b4e3
rzy <2356843722@qq.com>
2026-05-27 13:26:50 +0800

    Update AutoPlanApp.swift

--- AutoPlan/AutoPlanApp/AutoPlanApp.swift
+++ AutoPlan/AutoPlanApp/AutoPlanApp.swift
@@ -14,15 +14,6 @@ struct AutoPlanAppApp: App {
 
     init() {
         PromptBuilder.ensureCustomPromptFileExists()
-        // 获取主屏幕的 backingScaleFactor (比例系数)
-        if let mainScreen = NSScreen.main {
-            let screenScale = mainScreen.backingScaleFactor
-            print("Current screen scale factor: \(screenScale)")
-            let defaultBodySize: CGFloat = 13.0
-            let currentBodySize = NSFont.preferredFont(forTextStyle: .body).pointSize
-            let fontScale = currentBodySize / defaultBodySize
-            print("当前文字放大系数: \(fontScale)")
-        }
     }
 
     var body: some Scene {
